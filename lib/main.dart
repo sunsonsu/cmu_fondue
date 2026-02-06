3f591565e4aa998fef91971650be9c478abc3e0e
meaww2 <childenchaos@gmail.com>
2026-02-06 15:31:55 +0700

    refactor: Remove unused login, pre-login, and registor widgets; clean up imports in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/foundation.dart';
-import 'package:cmu_fondue/application/pages/dataconnect_test_querie_page.dart';
-import 'package:cmu_fondue/application/pages/login_page.dart';
-import 'package:cmu_fondue/application/pages/pokemon_page.dart';
-import 'package:cmu_fondue/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'package:cmu_fondue/firebase_options.dart';
+
 import 'package:cmu_fondue/data/datasources/firebase_auth_data_source.dart';
 import 'package:cmu_fondue/data/repositories/auth_repo_impl.dart';
+
+import 'package:cmu_fondue/domain/dataconnect_generated/generated.dart';
 import 'package:cmu_fondue/domain/usecases/login.dart';
 import 'package:cmu_fondue/domain/usecases/register.dart';
-import 'package:cmu_fondue/firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:cmu_fondue/domain/dataconnect_generated/generated.dart';
+
+// import 'package:cmu_fondue/application/pages/dataconnect_test_querie_page.dart';
+import 'package:cmu_fondue/application/pages/login_page.dart';
+import 'package:cmu_fondue/application/pages/pokemon_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
